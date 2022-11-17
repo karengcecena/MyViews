23365693001bc6bc312d04cf33090dfa061afffa
karengcecena <karengcecena@gmail.com>
2022-11-17 13:19:18 -0800

    Added more guest route testing: search friends, recommended, user profile, and log out all redirect user

--- tests.py
+++ tests.py
@@ -21,34 +21,47 @@ class FlaskTestsBasic(TestCase):
         result = self.client.get("/")
         self.assertIn(b"Login", result.data)
 
-    def test_search(self):
-        """Test guests can use the search bar for media"""
-        # can I test search if uses react????
-        pass
-    
-    def test_media_info(self):
-        """Tests guests can see media info for media search"""
-        pass
-
-    def test_media_info_rating(self):
-        """Tests guests cannot see rating in media info for media search"""
-        pass
-
     def test_friends_search(self):
         """Tests guests cannot see friends search bar"""
-        pass
+        
+        result = self.client.get("/search-friends")
+        self.assertIn(b"redirected", result.data)
 
     def test_your_recommended(self):
         """Tests guests cannot see recommended media page"""
-        pass
+        
+        result = self.client.get("/recommended")
+        self.assertIn(b"redirected", result.data)
 
     def test_user_profile(self):
         """Test guests cannot see a profile page"""
-        pass
+        
+        result = self.client.get("/user-profile")
+        self.assertIn(b"redirected", result.data)
 
     def test_log_out(self):
         """Tests guests cannot log out if not logged in"""
-        pass
+       
+        result = self.client.get("/logout")
+        self.assertIn(b"redirected", result.data)
+
+    def test_search(self):
+        """Test guests can use the search bar for media"""
+        # can I test search if uses react????
+        # result = self.client.get("/media-search-results-react")
+        # self.assertIn(b"Search a movie or tv show", result.data)
+    
+    def test_media_info(self):
+        """Tests guests can see media info for media search"""
+
+        # result = self.client.get("/media-info/movie/361743")
+        # self.assertIn(b"Top Gun: Maverick", result.data)
+
+    def test_media_info_rating(self):
+        """Tests guests cannot see rating in media info for media search"""
+        
+        # result = self.client.get("/media-info/movie/361743")
+        # self.assertNotIn(b"Rating", result.data)
 
 if __name__ == "__main__":
     # how do I get this page to run tests?
